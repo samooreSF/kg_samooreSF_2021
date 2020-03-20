006f177e9469dfe8cb191c92a7f514c15627e788
Satchel Moore <samoore@davidson.edu>
2020-03-20 16:57:36 -0500

    Create a dictionary that assigns key and values

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 
+function test(str1,str2) {
+  let dict = {};
+  for(let i of str1) {
+    if(dict.hasOwnProperty(i)) {
+      return false;
+    }
+    for(let j of str2) {
+      dict[i]=j;
+    }
+  }
+}
